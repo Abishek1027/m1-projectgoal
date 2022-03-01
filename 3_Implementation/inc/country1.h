ed66811be4e8883cc95d92c62926c228c8767cc7
Abishek1027 <98880999+Abishek1027@users.noreply.github.com>
2022-03-01 22:45:37 +0530

    Add files via upload

---
+++ 3_Implementation/inc/country1.h
@@ -0,0 +1,19 @@
+/**
+ * @file pound.h
+ * @author Abishek G
+ * @brief 
+ * @version 0.1
+ * @date 2022-03-01
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#ifndef __POUND_H
+#define __POUND_H
+
+#include "assert.h"
+#include "stdio.h"
+
+float pound(int x2, int x3);
+
+#endif
---
+++ 3_Implementation/inc/country2.h
@@ -0,0 +1,19 @@
+/**
+ * @file usd.h
+ * @author Abishek G
+ * @brief 
+ * @version 0.1
+ * @date 2022-03-01
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#ifndef __USD_H
+#define __USD_H
+
+#include "assert.h"
+#include "stdio.h"
+
+float usd(int x, int x1);
+
+#endif
